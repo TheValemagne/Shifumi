e240c77a0119f6e7ac68aa9488e90e58714f0db3
Jeremy Houde <jeremy.houde@laposte.net>
2023-12-28 12:55:42 +0100

    UI update

--- app/src/main/java/com/example/shifumi/fragment/StartScreenFragment.java
+++ app/src/main/java/com/example/shifumi/fragment/StartScreenFragment.java
@@ -3,7 +3,6 @@ package com.example.shifumi.fragment;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.shifumi.MainActivity;
-import com.example.shifumi.R;
 import com.example.shifumi.databinding.FragmentStartScreenBinding;
 import com.example.shifumi.fragment.listener.StartButtonListener;
 
@@ -21,14 +19,13 @@ import com.example.shifumi.fragment.listener.StartButtonListener;
  * create an instance of this fragment.
  */
 public class StartScreenFragment extends Fragment {
-    protected Button startButton;
 
     public StartScreenFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentStartScreenBinding binding = FragmentStartScreenBinding.inflate(inflater, container, false);
